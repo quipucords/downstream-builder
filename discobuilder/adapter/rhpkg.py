989a41bac9e5747da715a17280a2383ceafbb980
Brad Smith <brasmith@redhat.com>
2023-12-13 17:30:49 -0500

    feat: make rhpkg --target value optional

--- discobuilder/adapter/rhpkg.py
+++ discobuilder/adapter/rhpkg.py
@@ -1,8 +1,10 @@
 from discobuilder.adapter.subprocess import subprocess_call
 
 
-def rhpkg(command: str, target: str, repo_path: str, scratch=True):
-    args = ["rhpkg", command, "--target", target]
+def rhpkg(command: str, repo_path: str, target: str = None, scratch=True):
+    args = ["rhpkg", command]
+    if target:
+        args += ["--target", target]
     if scratch:
         args += ["--scratch"]
     subprocess_call(args, cwd=repo_path)
