e8ffe7e78ec839433470bbdcca829366d9302226
Brad Smith <brasmith@redhat.com>
2023-12-13 20:56:46 -0500

    refactor: move `rhpkg import` call to rhkpg module

--- discobuilder/adapter/rhpkg.py
+++ discobuilder/adapter/rhpkg.py
@@ -1,3 +1,4 @@
+from discobuilder import config
 from discobuilder.adapter.subprocess import subprocess_call
 
 
@@ -22,3 +23,13 @@ def container_build(repo_path, target: str = None, scratch=True):
     if scratch:
         args += ["--scratch"]
     subprocess_call(args, cwd=repo_path)
+
+
+def srpm_import(repo_path, srpm_path):
+    """Calls `rhpkg import` to update `sources` file with SRPM info."""
+    subprocess_call(
+        ["rhpkg", "import", srpm_path],
+        cwd=repo_path,
+        stdout=config.STDOUT,
+        stderr=config.STDERR,
+    )
