3c4d86714d3b19ed207d799092702d7ae86cae6d
Brad Smith <brasmith@redhat.com>
2025-07-11 15:48:46 -0400

    refactor: remove legacy server build option

--- discobuilder/builder/__init__.py
+++ discobuilder/builder/__init__.py
@@ -4,7 +4,6 @@ from discobuilder.adapter.git import configure_git
 from discobuilder.adapter.kerberos import kinit
 from discobuilder.builder.cli import build_cli  # noqa: F401
 from discobuilder.builder.installer import build_installer  # noqa: F401
-from discobuilder.builder.server import build_server  # noqa: F401
 
 
 def build():
@@ -14,8 +13,8 @@ def build():
     while True:
         choice = Prompt.ask(
             "What do you want to build?",
-            choices=["server", "cli", "installer"],
-            default="server",
+            choices=["cli", "installer"],
+            default="cli",
         )
         eval(f"build_{choice}()")
         if not Confirm.ask("Want to build something else?"):
--- discobuilder/config.py
+++ discobuilder/config.py
@@ -20,21 +20,6 @@ CHASKI_GIT_URL = environ.get(
 )
 CHASKI_GIT_REPO_PATH = environ.get("CHASKI_GIT_REPO_PATH", "/repos/chaski")
 CHASKI_GIT_COMMITTISH = environ.get("CHASKI_GIT_COMMITTISH", "main")
-# discovery-server is downstream repo for packaging quipucords
-DISCOVERY_SERVER_GIT_URL = environ.get(
-    "DISCOVERY_SERVER_GIT_URL",
-    "ssh://{username}@pkgs.devel.redhat.com/containers/discovery-server.git",
-)  # see also: https://pkgs.devel.redhat.com/cgit/containers/discovery-server
-DISCOVERY_SERVER_GIT_REPO_PATH = environ.get(
-    "DISCOVERY_SERVER_GIT_REPO_PATH", "/repos/discovery-server"
-)
-DISCOVERY_SERVER_GIT_REMOTE_RELEASE_BRANCH_DEFAULT = environ.get(
-    "DISCOVERY_SERVER_GIT_REMOTE_RELEASE_BRANCH_DEFAULT",
-    "remotes/origin/discovery-1-rhel-9",
-)
-DISCOVERY_SERVER_GIT_REMOTE_RELEASE_BRANCH_PREFIX = environ.get(
-    "DISCOVERY_SERVER_GIT_REMOTE_RELEASE_BRANCH_PREFIX", "remotes/origin/discovery-"
-)
 # discovery-cli is downstream repo for packaging qpc
 DISCOVERY_CLI_GIT_URL = environ.get(
     "DISCOVERY_CLI_GIT_URL",
